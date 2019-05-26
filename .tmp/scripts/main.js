2e91ff1bd2360c3a5da3a3d517cab857ee9deb13
La Fountain <github email address>
2019-05-26 11:50:40 -0400

    add main functions

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,4 +1,67 @@
 "use strict";
 
-console.log('\'Allo \'Allo!');
+console.log("'Allo 'Allo!");
+/**
+ *  Listener for load
+ */
+
+window.addEventListener("load", function () {
+  if (typeof web3 !== "undefined") {
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.log("Injected web3 Not Found!!");
+    var provider = document.getElementById("provider_url").value;
+    window.web3 = new Web3(new Web3.providers.HttpProvider(provider));
+  }
+
+  startApp();
+});
+/**
+ * This method is called for connecting to the node
+ * The provider url is provideed in a document element with the
+ * id = provider_url
+ */
+
+startApp = function startApp() {
+  doFilterStopWatching();
+  doContractEventWatchStop();
+
+  if (web && web3.isConnected()) {
+    setData("connect_status", "Connected", false);
+    setWeb3Version();
+    if (autoRetrieveFlag) doGetAccounts();
+  } else {
+    setData("connect_status", "Not Connected", true);
+  }
+
+  if (!autoRetrieveFlag) return; //  doConnect();
+  //  doGetAccounts();
+
+  doGetNodeStatus();
+  doGetCompliers();
+};
+
+doConnect = function doConnect() {
+  var provider = document.getElementById("provider_url").value;
+  window.web3 = new Web3(new Web3.providers.HttpProvider(provider));
+  startApp();
+};
+
+setWeb3Version = function setWeb3Version() {
+  web3.version.getNode(function (error, result) {
+    if (error) setData("version_information", error, true);else {
+      setData("version_information", result, false);
+
+      if (result.toLowerCase().includes("metamask")) {
+        nodeType = "metamask";
+      } else if (result.tooLowerCase().includes("rinkebyrpc")) {
+        nodeType = "rinkebyrpc";
+      } else {
+        nodeType = "geth";
+      }
+
+      setUIBasedOnNodeType();
+    }
+  });
+};
 //# sourceMappingURL=main.js.map
